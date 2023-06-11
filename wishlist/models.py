b0ad5c372522cfa5cd2bb6692bfcaea422693451
Rossanne <rossanneh@gmail.com>
2023-06-11 13:59:41 +0000

    Fix corrupted database

--- wishlist/models.py
+++ wishlist/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from products.models import Product
 
 
-class ItemWishlist(models.Model):
+class UserWishlist(models.Model):
 
-    item_name = models.CharField(max_length=50, null=True)
-    item_price = models.IntegerField(null=True)
-    description = models.TextField(null=True)
+    item_name = models.CharField(max_length=50,)
+    item_price = models.IntegerField()
+    description = models.TextField()
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from wishlist.models import ItemWishlist
+from wishlist.models import UserWishlist
 
 
 def show_wishlist(request):
 
-    data_wishlist_item = ItemWishlist.objects.all()
+    data_wishlist_item = UserWishlist.objects.all()
     context = {
         'list_item': data_wishlist_item,
         'name': 'Ross'
