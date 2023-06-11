b435bedb123cd7bc28cd358ad8c0b10b8c021774
Rossanne <rossanneh@gmail.com>
2023-06-11 14:35:34 +0000

    Add new wishlist setup

--- wishlist/models.py
+++ wishlist/models.py
@@ -3,8 +3,18 @@ from django.contrib.auth.models import User
 from products.models import Product
 
 
+# Delete the model below eventually and json / everything associated with it
 class UserWishlist(models.Model):
 
     item_name = models.CharField(max_length=50,)
     item_price = models.IntegerField()
     description = models.TextField()
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    wished_item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.wished_item
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from wishlist.models import UserWishlist
+from wishlist.models import Wishlist
 
 
 def show_wishlist(request):
 
-    data_wishlist_item = UserWishlist.objects.all()
+    data_wishlist_item = Wishlist.objects.all()
     context = {
         'list_item': data_wishlist_item,
-        'name': 'Ross'
     }
 
     return render(request, "wishlist.html", context)
