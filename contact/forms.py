b15f02300f2afd085147988e68d0b742976b931f
Rossanne <rossanneh@gmail.com>
2023-06-28 21:53:38 +0000

    Edit field names on commission form

--- contact/forms.py
+++ contact/forms.py
@@ -7,3 +7,15 @@ class CommissionForm(forms.ModelForm):
     class Meta:
         model = Commission
         exclude = ['user', 'status']
+
+    def __init__(self, *args, **kwargs):
+        super(CommissionForm, self).__init__(*args, **kwargs)
+        self.fields['name'].label = "Enter your name"
+        self.fields['email'].label = "Enter your email"
+        self.fields['description'].label = "Tell us about what you'd like the finished piece to look like"
+        self.fields['dimensions'].label = "What size would you like it to be?"
+        self.fields['style'].label = "What style would you like? (E.g. classical / surrealist)"
+        self.fields['artist'].label = "Which artist (if any) would you like it to be in the style of?"
+        self.fields['framed'].label = "Tick if you'd like it to be framed"
+        self.fields['medium'].label = "What medium would you like? (E.g. watercolour / oil on canvas)"
+        self.fields['dominant_colours'].label = "Do you have any particular colour-palette or dominant colours in mind?"
