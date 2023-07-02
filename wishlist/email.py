ea7f941c85047794961d911f5ee3f6e5b5140cd4
Rossanne <rossanneh@gmail.com>
2023-07-02 19:09:58 +0000

    Test new email

--- wishlist/email.py
+++ wishlist/email.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 
 def send_sale_alert_email(user, product):
     subject = f"The artwork {product.name} is now on sale!"
-    message = f"The product {product.name}, which is on your wishlist, \
-        is now on sale! Buy it at a discount whilst stocks last"
+    message = (
+        f"The product {product.name}, which is on your wishlist, "
+        "is now on sale! Buy it at a discount whilst stocks last, "
+        "at https://eponymous-bosch-b9b71a6bff59.herokuapp.com/")
     recipient_list = [user.email]
     send_mail(subject, message, 'eponymous_bosch@example.com', recipient_list)
