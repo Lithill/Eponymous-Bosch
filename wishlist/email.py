aba1e0b9b9ff900e9a20315d63456b455896a0de
Rossanne <rossanneh@gmail.com>
2023-07-02 19:15:41 +0000

    Improve email message

--- wishlist/email.py
+++ wishlist/email.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 def send_sale_alert_email(user, product):
     subject = f"The artwork {product.name} is now on sale!"
     message = (
-        f"The product {product.name}, which is on your wishlist, "
+        f"The product '{product.name}', which is on your wishlist, "
         "is now on sale! Buy it at a discount whilst stocks last, "
-        "at https://eponymous-bosch-b9b71a6bff59.herokuapp.com/")
+        "at https://eponymous-bosch-b9b71a6bff59.herokuapp.com/wishlist/.")
     recipient_list = [user.email]
     send_mail(subject, message, 'eponymous_bosch@example.com', recipient_list)
