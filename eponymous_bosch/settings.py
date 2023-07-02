b3c6c15bee6274654609a032e6b92620804756e5
Rossanne <rossanneh@gmail.com>
2023-07-02 12:02:10 +0000

    Add ability to runserver from gitpod if needed

--- eponymous_bosch/settings.py
+++ eponymous_bosch/settings.py
@@ -133,16 +133,19 @@ WSGI_APPLICATION = 'eponymous_bosch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL', ''))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-DATABASES = {
-   'default': dj_database_url.parse(os.environ.get('DATABASE_URL', ''))
-}
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
